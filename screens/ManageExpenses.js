4ee833f77e61542aa604e62e1eb796a04197e127
ASUS <eb.mehrdad@gmail.com>
2022-04-10 12:53:48 +0430

    screens and navigate lib created

---
+++ screens/ManageExpenses.js
@@ -0,0 +1,18 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ManageExpenses = () => {
+ 
+  return (
+   <View style={styles}>
+    <Text></Text>
+   </View>
+  );
+ 
+}
+
+const styles = StyleSheet({
+});
+
+export default ManageExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,18 @@
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const RecentExpenses = () => {
+ 
+  return (
+   <View style={styles}>
+    <Text></Text>
+   </View>
+  );
+ 
+}
+
+const styles = StyleSheet({
+});
+
+export default RecentExpenses;
